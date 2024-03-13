418b2731e1cb18e739ed9199d926946add079e9a
tomcombriat <tomcombriat@live.fr>
2024-03-13 22:05:58 +0100

    Simpler analoginput, always outputting on the same range

--- AnalogInput.h
+++ AnalogInput.h
@@ -1,6 +1,8 @@
 #ifndef _ANALOGINPUT_
 #define _ANALOGINPUT_
 
+#include<FixMath.h>
+
 /*
   Combriat 2018, 2019, 2020, 2023, 2024
   This file is part of the OS embedded in the e-sax - TES
@@ -61,8 +63,105 @@ private:
 
   uint16_t input_range = 0;
     
-    
+};
 
+template<uint8_t NBits>  // always output on the full range of int16_t
+class AnalogInput2
+{
+public:
+  AnalogInput2(const int _pin, const unsigned long _response_time, const int16_t _dead_zone=10, const int16_t _safety = 10):pin(_pin), response_time(_response_time), dead_zone(_dead_zone), safety(_safety)
+  {
+    pinMode(pin, INPUT);
+    // analogReadResolution(NBits);
+  }
+
+  void calibrate()
+  {
+    int32_t tamp_bias = 0;
+    for (byte i=0;i<16;i++) tamp_bias += analogRead(pin);
+    bias = tamp_bias >> 4;
+  }
+
+  int16_t value() {return value;}
+
+  void setInvert(bool _inverted) {inverted = _inverted;}
+
+  bool update()
+  {
+    changed = false;
+    if (millis() - last_read_time > response_time)
+      {
+	last_read_time = millis();
+	int32_t internal_value = analogRead(pin) - bias;
+	
+	if (internal_value > dead_zone)
+	  {
+	    internal_value -= dead_zone;
+	    int32_t scaled_value = (UFix<NBits-1,0>(internal_value) * scaling_factor_pos).asInt();
+	    
+	    if (scaled_value > 32767) scaled_value = 32767;
+	    value = scaled_value;
+	  }
+
+	else if (internal_value < -dead_zone)
+	  {
+	    internal_value += dead_zone;
+	    int32_t scaled_value = -(UFix<NBits-1,0>(-internal_value) * scaling_factor_neg).asInt();
+	    if (scaled_value < -32768) scaled_value = -32768;
+	    value = scaled_value;
+	  }	
+	else value = 0;
+	
+	if (inverted) value = 65535 - value;
+      }
+    return changed;
+  
+  }
+
+  void deCalibrate()
+  {
+    max_sensor_value = 0;
+    min_sensor_value = 65535;
+  }
+
+  void calibrateMinMax()
+  {
+    int16_t tamp_value = analogRead(pin);
+
+    if (tamp_value < min_sensor_value) min_sensor_value = tamp_value;
+    if (tamp_value > max_sensor_value) max_sensor_value = tamp_value;
+  }
+
+  void setMinMaxSensor(uint16_t min, uint16_t max)
+  {
+    min_sensor_value = min;
+    max_sensor_value = max;
+  }
+
+  uint16_t getMinSensor() {return min_sensor_value;}
+  uint16_t getMaxSensor() {return max_sensor_value;}
+
+  void computeScalingFactor()
+  {
+    scaling_factor_pos = ((toUInt(uint16_t(max_sensor_value - safety - dead_zone - bias))).invAccurate()).sL<15>();
+    scaling_factor_neg = ((toUInt(uint16_t(bias - dead_zone - safety - min_sensor_value))).invAccurate()).sL<15>();
+    
+  }
+  
+  
+
+
+private:
+  uint16_t value, max_sensor_value = 3500, min_sensor_value = 50;
+  uint16_t bias;
+  const int pin;
+  const unsigned long response_time;
+  unsigned long last_read_time=0;
+  const uint16_t dead_zone;
+  const uint16_t safety;
+  bool inverted, changed;
+  UFix<16,16> scaling_factor_neg, scaling_factor_pos;
+  
 
 };
 
